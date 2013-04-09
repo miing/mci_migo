ee44bea77886fa3b327af64f83f4d8eadfaa8540
miing <samuel.miing@gmail.com>
2013-04-10 00:24:55 +0800

    Merge: rev767 (Cut 13.04.04 release)

--- identityprovider/__init__.py
+++ identityprovider/__init__.py
@@ -1,4 +1,4 @@
 # Copyright 2010 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-__version__ = '13.03.21-r741'
+__version__ = '13.04.04-r766'
