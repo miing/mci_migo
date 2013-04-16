02b75802189fc997c3a95802a451c5054186bd1c
miing <samuel.miing@gmail.com>
2013-04-17 00:29:18 +0800

    Cut 13.04.16 release

--- identityprovider/__init__.py
+++ identityprovider/__init__.py
@@ -1,4 +1,4 @@
 # Copyright 2010 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-__version__ = '13.04.10-r780'
+__version__ = '13.04.16-r798'
